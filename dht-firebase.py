c841256e85bb36ac003d8b77cd192222faad3ba2
mawin arnphanit <30917679+MawinAPN@users.noreply.github.com>
2020-03-27 20:38:25 +0700

    Add files via upload

---
+++ dht-firebase.py
@@ -0,0 +1,33 @@
+import RPi.GPIO as GPIO
+from time import sleep
+import datetime
+from firebase import firebase
+
+import urllib2, urllib, httplib
+import json
+import os 
+from functools import partial
+firebase = firebase.FirebaseApplication('INSERT YOUR FIREBASE HERE', None)
+
+
+def update_firebase():
+
+	i = 1
+	while i < 6:
+	 firebase.post('/TEST',i)
+	i += 1
+
+while True:
+		update_firebase()
+		
+        #sleepTime = int(sleepTime)
+		sleep(5)
+	
+
+
+
+
+
+
+
+
